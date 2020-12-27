c876be846cc434aa060d51632c84c708283ab97f
Pandakidz2 <67604195+Pandakidz2@users.noreply.github.com>
2020-12-27 17:36:13 -0500

    API call to google servers

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,15 @@ package src;
 
 public class Main{
     public static void main(String args[]){
-        Window frame = new Window(700, 700);
+        /*Window frame = new Window(700, 700);
         
         frame.panelSetup();
         frame.setVisible(true);
         frame.update();
+        */
+        
+        Update.points();
+        Update.JSON();
     }
 
 }
